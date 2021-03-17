6c4ee7d0d9d386d91b469d738e172237df76ab56
Jon Hurst <jon.a@hursts.org.uk>
2021-03-17 08:33:11 +0000

    Handle edge cases

--- night.py
+++ night.py
@@ -8,6 +8,7 @@ import astral.sun
 
 from airport_nvecs import airfields
 
+
 def recursive_bisect(nvec1, nvec2, depth):
     assert(depth >= 0)
     added = [nvec1[N] + nvec2[N] for N in range(3)]
@@ -62,38 +63,51 @@ def night_p(nvec, dt):
     return True
 
 
-def night_duration(_from, _to, _start, _end, seclength=10):
+def night_duration(_from, _to, _start, _end, seclength=32):
     flight_duration = (_end - _start).total_seconds() / 60
     min_sections = flight_duration / seclength
     depth = math.ceil(math.log(min_sections) / math.log(2)) - 1
     nvecs = recursive_bisect(_from, _to, depth)
     time_d = (_end - _start) / (len(nvecs) - 1)
+    time_d_mins = time_d.total_seconds() / 60
     time = _start
-    last_nstatus = night_p(nvecs[0], time)
+    last_nvec = nvecs[0]
+    last_nstatus = night_p(last_nvec, time)
     night_acc = 0.0
     for nvec in nvecs[1:]:
         time += time_d
         nstatus = night_p(nvec, time)
-        if nstatus:
-            if last_nstatus:
-                night_acc += time_d.total_seconds() / 60
-            else: # day to night
-                night_acc += time_d.total_seconds() / 120
-        else: #night to day
-            if last_nstatus:
-                night_acc += time_d.total_seconds() / 120
+        if nstatus != last_nstatus:#day to night or night to day
+            if seclength > 2:
+                night_acc += night_duration(last_nvec, nvec, time - time_d, time, 2)
+            else:
+                night_acc += time_d_mins / 2
+        elif nstatus:#night at current nvec
+            night_acc += time_d_mins
         last_nstatus = nstatus
+        last_nvec = nvec
+        print(nvec, ",")
     return round(night_acc)
 
 
 def test():
     _from = airfields["BSL"]
     _to = airfields["BRS"]
-    _start = datetime.datetime(202, 12, 20, 15, 31)
+    _start = datetime.datetime(2020, 12, 20, 15, 31)
     _end = _start + datetime.timedelta(minutes=116)
-    for c in range(1000):
-        duration = night_duration(_from, _to, _start, _end)
-    print(duration)
+    duration = night_duration(_from, _to, _start, _end)
+    print("BSL-BRS:", duration)
+    print("KTT-BRS:", night_duration(airfields["KTT"],
+                                     airfields["BRS"],
+                                     datetime.datetime(2019, 12, 21, 11, 59),
+                                     datetime.datetime(2019, 12, 21, 15, 21)))
+    print("polar", night_duration((0,0,1), (0,0,-1),
+                                  datetime.datetime(2019, 12, 1, 11, 59),
+                                  datetime.datetime(2019, 12, 2, 11, 59)))
+    print("equitorial", night_duration((1,0,0), (-1,0,0),
+                                  datetime.datetime(2019, 12, 1, 11, 59),
+                                  datetime.datetime(2019, 12, 2, 11, 59)))
+
 
 if __name__ == "__main__":
     test()
