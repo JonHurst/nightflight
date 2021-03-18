27056e51f4cdb39324a84ed11126648a9fc5ec3e
Jon Hurst <jon.a@hursts.org.uk>
2021-03-18 17:17:46 +0000

    Add __init__.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nightflight",
-    version="0.1",
+    version="0.2",
     author="Jon Hurst",
     author_email="jon.a@hursts.org.uk",
     description="Calculate night flying hours",
