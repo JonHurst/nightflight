24d533bf45b771a20ea994e9e170253fa6893150
Jon Hurst <jon.a@hursts.org.uk>
2021-03-18 09:34:55 +0000

    Add nvecs for some small airfields

--- make_locations.py
+++ make_locations.py
@@ -39,6 +39,15 @@ def main():
         airfields[f[F_IATA]] = to_nvec(lat, long)
     #add missing airfields
     airfields["BER"] = to_nvec(52.36667, 13.50333)
+    airfields["SZD"] = to_nvec(53.39417, -1.3886)
+    airfields["EGCK"] = to_nvec(53.10167, -4.3375)
+    airfields["EGCW"] = to_nvec(52.62944, -3.1525)
+    airfields["EGCB"] = to_nvec(53.47167, -2.3897)
+    airfields["EGBG"] = to_nvec(52.60778, -1.0319)
+    airfields["EGTF"] = to_nvec(51.34806, -0.5586)
+    airfields["EGBW"] = to_nvec(52.19222, -1.6144)
+    airfields["DOC"] = to_nvec(57.87217, -4.0263)
+    airfields["Langford L"] = to_nvec(54.623, -6.3)
     #output date to stdout
     print("airfields = {")
     print(",\n".join([f"'{i}': {airfields[i]}".replace("\\", "\\\\")
