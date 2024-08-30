593fee7ca4e84ad33d3a45c224aae2c67e3b467f
Jon Hurst <jon.a@hursts.org.uk>
2024-08-30 10:30:57 +0100

    Changed over to ourairports.com for airport location data, and include ICAO codes

--- nightflight/make_locations.py
+++ nightflight/make_locations.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
-"""Utility script to process airports.dat file
+"""Utility script to process airports.csv file
 
 This file is found at:
 
-  https://openflights.org/data.html
-
-Pipe it to stdin, get a python file defining airfields dictionary, keyed with
-IATA designator giving nvec (ordered triple) on stdout.
+  https://ourairports.com/data/
 
 """
 
 import sys
 import math
 import csv
+import re
 
 #fields
-F_IATA = 4
-F_LAT = 6
-F_LONG = 7
+F_LAT = 4
+F_LONG = 5
+F_ICAO = 1
+F_IATA = 13
 
 
 def to_nvec(lat, long):
@@ -31,27 +30,21 @@ def to_nvec(lat, long):
 
 
 def main():
-    airfields = {}
-    #read file from stdin
-    for f in csv.reader(sys.stdin):
+    lines = csv.reader(sys.stdin)
+    headers = next(lines)
+    re_ICAO = re.compile(r"[A-Za-z]{4}")
+    re_IATA = re.compile(r"[A-Za-z]{3}")
+    for f in lines:
         lat = float(f[F_LAT])
         long = float(f[F_LONG])
-        airfields[f[F_IATA]] = to_nvec(lat, long)
-    #add missing airfields
-    airfields["BER"] = to_nvec(52.36667, 13.50333)
-    airfields["SZD"] = to_nvec(53.39417, -1.3886)
-    airfields["EGCK"] = to_nvec(53.10167, -4.3375)
-    airfields["EGCW"] = to_nvec(52.62944, -3.1525)
-    airfields["EGCB"] = to_nvec(53.47167, -2.3897)
-    airfields["EGBG"] = to_nvec(52.60778, -1.0319)
-    airfields["EGTF"] = to_nvec(51.34806, -0.5586)
-    airfields["EGBW"] = to_nvec(52.19222, -1.6144)
-    airfields["DOC"] = to_nvec(57.87217, -4.0263)
-    airfields["Langford L"] = to_nvec(54.623, -6.3)
-    #output date to stdout
-    print("airfields = {")
-    print(",\n".join([f"'{i}': {airfields[i]}".replace("\\", "\\\\")
-                      for i in sorted(airfields)]))
-    print("}")
+        icao, iata = "", ""
+        if re_ICAO.match(f[F_ICAO]):
+            icao = f[F_ICAO].upper()
+        if re_IATA.match(f[F_IATA]):
+            iata = f[F_IATA].upper()
+        nvec = [f"{round(X * 10000)}" for X in to_nvec(lat, long)]
+        if icao or iata:
+            print(f"{icao},{iata},{','.join(nvec)}")
+
 
 if __name__ == "__main__": main()
